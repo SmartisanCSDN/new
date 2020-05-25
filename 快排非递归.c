506912b9b8aeba88eb76da51921011087bbac7e1
半分醉 <535726496@qq.com>
2020-05-25 11:39:03 +0800

    快排非递归

---
+++ 快排非递归.c
@@ -0,0 +1,30 @@
+//øÏ≈≈∑«µ›πÈ
+void QuickSort_N(int* value, int start, int end)
+{
+	assert(value != NULL);
+	Stack st;
+	StackInit(&st);
+	if (start < end)
+	{
+		StackPush(&st, start);
+		StackPush(&st, end);
+	}
+	while (st.top != NULL)
+	{
+		int right = StackTop(&st);
+		StackPop(&st);
+		int left = StackTop(&st);
+		StackPop(&st);
+		int index = OnceQuickSort(value, left, right);
+		if (left < index - 1)
+		{
+			StackPush(&st, left);
+			StackPush(&st, index - 1);
+		}
+		if (index + 1 < right)
+		{
+			StackPush(&st, index + 1);
+			StackPush(&st, right);
+		}
+	}
+}
