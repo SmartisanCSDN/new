6dab17327b1dd72cc450898bce4462737b4e66a9
lwgithubcode <535726496@qq.com>
2020-08-26 21:44:27 +0800

    练习

---
+++ 8-26练习/test2.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	int count = 0;
+//	int prodect = 1;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		prodect *= i;
+//		while (prodect % 10 == 0)
+//		{
+//			count++;
+//			prodect /= 10;
+//		}
+//		prodect %= 100000;
+//	}
+//	cout << count << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	while (cin >> n)
+//	{
+//		int pro = 1;
+//		for (int i = 1; i <= n; i++)
+//			pro *= i;
+//		cout << pro << endl;
+//	}
+//	return 0;
+//}
